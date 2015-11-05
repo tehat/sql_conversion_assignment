514ab35e754d43c2f3458f943f906a0d245a6c5b
Kris Szafranski <kdszafranski@gmail.com>
2015-11-05 16:01:38 -0600

    converted addPerson and get data routes

--- server/app.js
+++ server/app.js
@@ -4,48 +4,69 @@ var app = express();
 var path = require('path');
 var bodyParser = require('body-parser');
 
-var mongoose = require('mongoose');
-var Schema = mongoose.Schema;
+var pg = require('pg');
+var connectionString = process.env.DATABASE_URL || 'postgres://localhost:5432/sql_lecture';
 
-mongoose.connect('mongodb://localhost/mongo_lecture');
-mongoose.model('Person', new Schema({"name": String, "location": String}, {collection: 'people'}));
-var Person = mongoose.model('Person');
-
-app.set("port", process.env.PORT || 5000);
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({expanded: true}));
 
+// Get all the people information
 app.get('/data', function(req,res){
-    var query = req.query.peopleSearch;
+    var results = [];
 
-    if(query){
-        Person.find({"name" : query}, function(err, data){
-            if(err){
-                console.log("ERROR! : ", err);
-            }
-            res.send(data);
+    //SQL Query > SELECT data from table
+    pg.connect(connectionString, function (err, client, done) {
+        var query = client.query("SELECT id, name, location FROM people ORDER BY name ASC");
+
+        // Stream results back one row at a time, push into results array
+        query.on('row', function (row) {
+            results.push(row);
         });
-    } else {
-        Person.find({}, function(err, data){
-            if(err){
-                console.log("ERROR! : ", err);
-            }
-            res.send(data);
+
+        // After all data is returned, close connection and return results
+        query.on('end', function () {
+            client.end();
+            return res.json(results);
         });
-    }
+
+        // Handle Errors
+        if (err) {
+            console.log(err);
+        }
+    });
 });
 
+// Add a new person
 app.post('/data', function(req,res){
     console.log(req);
-    var addedPerson = new Person({
+
+    var addedPerson = {
         "name" : req.body.peopleAdd,
         "location" : req.body.locationAdd
+    };
+
+    pg.connect(connectionString, function (err, client) {
+        //SQL Query > Insert Data
+        //Uses prepared statements, the $1 and $2 are placeholder variables. PSQL then makes sure they are relatively safe values
+        //and then uses them when it executes the query.
+
+        //var query = "INSERT INTO people (name, location) VALUES ('" + addedPerson.name + "', '" + addedPerson.location + "')";
+        //console.log(query);
+        //client.query(query);
+
+        //client.query("INSERT INTO people (name, location) VALUES ($1, $2)", [addedPerson.name, addedPerson.location]);
+        client.query("INSERT INTO people (name, location) VALUES ($1, $2) RETURNING id", [addedPerson.name, addedPerson.location],
+            function(err, result) {
+                if(err) {
+                    console.log("Error inserting data: ", err);
+                    res.send(false)
+                }
+
+                res.send(true);
+            });
+
     });
 
-    addedPerson.save(function(err, data){
-        if(err) console.log(err);
-        res.send(data);
-    });
 });
 
 app.delete('/data', function(req,res){
@@ -64,6 +85,7 @@ app.get("/*", function(req,res){
     res.sendFile(path.join(__dirname, "./public", file));
 });
 
+app.set("port", process.env.PORT || 5000);
 app.listen(app.get("port"), function(){
     console.log("Listening on port: ", app.get("port"));
 });
